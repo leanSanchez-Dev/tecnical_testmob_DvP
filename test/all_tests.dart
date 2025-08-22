ec6fea2a33dbf0993b48cfd3f8391e7cf74b5cde
leanSanchez-Dev <leansanchez14@gmail.com>
2025-08-21 23:36:51 -0500

    solucion de error

--- test/all_tests.dart
+++ test/all_tests.dart
@@ -7,7 +7,6 @@ import 'providers/user_provider_test.dart' as user_provider_tests;
 import 'widgets/custom_input_field_test.dart' as custom_input_tests;
 import 'screens/user_profile_screen_test.dart' as profile_screen_tests;
 import 'integration/full_flow_test.dart' as integration_tests;
-import 'utils/constants_test.dart' as constants_tests;
 import 'widget_test.dart' as widget_tests;
 
 void main() {
@@ -28,10 +27,6 @@ void main() {
     group('Screens Tests', () {
       profile_screen_tests.main();
       widget_tests.main();
-    });
-
-    group('Utils Tests', () {
-      constants_tests.main();
     });
 
     group('Integration Tests', () {
